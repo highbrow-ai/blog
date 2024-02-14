275189833502443feccd00423c231d93bdb19368
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2024-02-15 04:06:11 +0530

    Adding env variables to config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/blog',
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  env: {
+    MICROCMS_API_KEY: 'cwJSEyPIqxIbSsTeXWlhtPde2bzLnpd88dtX',
+    MICROCMS_SERVICE_DOMAIN: 'jqyxa0b0wm',
+    BASE_URL: 'https://highbrow-ai.github.io/blog/',
+  },
 };
 
 module.exports = nextConfig;
