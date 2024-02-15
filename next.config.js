2134c82752e37f440ce0ccef609465edd786d3e3
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2024-02-15 12:01:32 +0530

    Update configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <=== enables static exports
   reactStrictMode: true,
   env: {
     MICROCMS_API_KEY: 'cwJSEyPIqxIbSsTeXWlhtPde2bzLnpd88dtX',
